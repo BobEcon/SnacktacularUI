8337dddaf39a1149628d5df1181d430342fdb50d
Robert Beachill <bbeachill@icloud.com>
2025-06-12 23:16:33 +0100

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Robert Beachill on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
